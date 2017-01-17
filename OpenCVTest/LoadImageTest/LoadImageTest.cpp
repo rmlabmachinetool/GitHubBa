31d894203b0b097fafb497ce8f9bd42e38ac6955
DELLXPS8700D205 <DELLXPS8700D205@D3-402-01>
2017-01-17 16:22:15 +0900

    Test on 2017/01/17

--- OpenCVTest/LoadImageTest/LoadImageTest.cpp
+++ OpenCVTest/LoadImageTest/LoadImageTest.cpp
@@ -5,28 +5,37 @@
 //
 #include "stdafx.h"
 #include <opencv2/core/core.hpp>
-#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <string>
+#include "microtimer.h"
 using namespace cv;
 using namespace std;
 int main(int argc, char** argv)
 {
+	//Excution timer
+	//microtimer            microtime;
+	//microtimer::size_type start, end, end1, end2, end3, end4, end5, end6;
+
 	string imageName("test.jpg"); // by default
+	//imageName = "E:/10.JPG";
 	if (argc > 1)
 	{
 		imageName = argv[1];
 	}
-	Mat image;
+	Mat image, imageOriginal;
 	image = imread(imageName.c_str(), IMREAD_COLOR); // Read the file
+	imageOriginal = image;
 	if (image.empty())                      // Check for invalid input
 	{
 		cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
 	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+
 	imshow("Display window", image);                // Show our image inside it.
+	
 
 	waitKey(0); // Wait for a keystroke in the windowd
 	return 0;
